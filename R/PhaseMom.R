1745b582235f5482a9893b3bfdc82058fc53385d
yangjl0930@gmail.com <yangjl0930@gmail.com>
2015-10-12 18:13:19 -0700

    start to document PhaseMom

--- R/PhaseMom.R
+++ R/PhaseMom.R
@@ -1,3 +1,33 @@
+#'
+#' \code{Phasing mom's genotype. } 
+#'
+#' Phasing mom's genotype can be conducted using confident genotyping data or 
+#' using imputed  genotype data.
+#'
+#' @param obs_mom A vector of mom's GBS data. Coded with 0, 1 and 2, which is the copy of alternative alleles. 
+#' Missing data should be coded with 3.
+#' @param obs_kids A list of vectors of Kid's GBS data. Coded with 0, 1 and 2, which is the copy of alternative alleles. 
+#' Missing data should be coded with 3.
+#' @param hom.error Homozygous error rate, default=0.02.
+#' @param het.error Heterozygous error rate, default=0.8.
+#' @param oddratio The cutoff used for determining the log likelihood ratio of the highest and the 2nd highest genotypes. 
+#' The oddratio = NULL means to report the most likely genotype. Default value sets to 0.5.
+#' @param returnall For function 'genomom', returnall=TRUE will return with all the information; returnall=FALSE will return
+#' mom's genotype only.
+#' @return return a data.frame with all the log likelihoods. Using function \code{momgeno} to extract mom's genotype. 
+#'   
+#'   See \url{https://github.com/yangjl/imputeR/blob/master/vignettes/imputeR-vignette.pdf} for more details.
+#' @examples
+#' obs_mom <- c(0, 0, 0)
+#' obs_kids <- list(c(0, 0, 0), c(0, 0, 0), c(0, 0, 0), c(0, 0, 0), c(0, 0, 0), c(0, 0, 0),
+#' c(0, 0, 0), c(0, 0, 0), c(0, 0, 0), c(0, 0, 0), c(0, 0, 0), c(0, 0, 0))
+#'
+#' impute_mom(obs_mom, obs_kids, hom.error=0.02, het.error=0.8)
+#' 
+#' momgeno(geno, oddratio=0.5, returnall=FALSE)
+#' 
+#' 
+
 ############################################################################
 # Get mom's phase
 # should return two haps
