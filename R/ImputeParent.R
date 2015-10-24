e3f84e06656cd48e47001893c8625ccb3a0c9550
Jeff Ross-Ibarra <rossibarra@gmail.com>
2015-10-24 15:03:42 -0700

    changing mom to parent in text throughout. no changes to likelihoods or real code base yet.

---
+++ R/ImputeParent.R
@@ -0,0 +1,105 @@
+#'
+#' \code{Impute parent's genotype using parent and kids' GBS data. } 
+#'
+#' We have observed parent and observed kids. We want to know the likelihood of the parent's possible genotypes. 
+#' This function is to impute the parent's most likely genotype from a progeny array of k kids by 
+#' giving a log likelihood threshold.
+#'
+#' @param obs_parent A vector of the current observed parent's GBS data. Coded with 0, 1 and 2, which is the copy of alternative alleles. 
+#' Missing data should be coded with 3.
+#' @param obs_kids A list of vectors of Kid's GBS data. Coded with 0, 1 and 2, which is the copy of alternative alleles. 
+#' Missing data should be coded with 3.
+#' @param hom.error Homozygous error rate, default=0.02.
+#' @param het.error Heterozygous error rate, default=0.8.
+#' @param p vector of allele frequencies at each locus. should be estimated from set of all parents.
+#' WARNING: if not supplied, a random neutral SFS is used to generate p at each locus.
+#' @param oddratio The cutoff used for determining the log likelihood ratio of the highest and the 2nd highest genotypes. 
+#' The oddratio = NULL means to report the most likely genotype. Default value sets to 0.6931472, 
+#' which is equivalent to most likely genotype being twice as likely as next most likely. 2X as likely
+#' @param returnall For function 'parentgeno', returnall=TRUE will return with all the information; returnall=FALSE will return
+#' parent's genotype only (either maxlikelihood or, if oddratio is specified, genotypes with low oddratios will be set to missing).
+#' @return return a data.frame with all the log likelihoods. Using function \code{parentgeno} to extract parent's genotype. 
+#'   
+#'   See \url{https://github.com/yangjl/imputeR/blob/master/vignettes/imputeR-vignette.pdf} for more details.
+#' @examples
+#' obs_parent <- c(0, 0, 0)
+#' obs_kids <- list(c(0, 0, 0), c(0, 0, 0), c(0, 0, 0), c(0, 0, 0), c(0, 0, 0), c(0, 0, 0),
+#' c(0, 0, 0), c(0, 0, 0), c(0, 0, 0), c(0, 0, 0), c(0, 0, 0), c(0, 0, 0))
+#'
+#' impute_parent(obs_parent, obs_kids, hom.error=0.02, het.error=0.8)
+#' 
+#' parentgeno(geno, oddratio=0.5, returnall=FALSE)
+#' 
+#' 
+impute_parent <- function(obs_parent, obs_kids, hom.error=0.02, het.error=0.8,p=NULL){
+    
+    ### need to check genotypes
+    numloci <- length(obs_parent)
+    
+    message(sprintf("###>>> impute parent's genotype using [ %s ] kids ...", length(obs_kids)))
+    ### get probability matrices
+    gen_error <- gen_error_mat(hom.error, het.error)
+    probs <- error_mx(hom.error, het.error)
+    
+    ### make sfs if not provided?
+    if(is.null(p)){
+        message(sprintf("###>>> no allele frequencies provided. generating random allele frequencies from a neutral SFS"))
+        sfs <- getsfs(x = 1:99/100)
+        p <- sample(sfs, numloci,replace=TRUE) #get freqs for all loci
+    }
+    
+    res <- lapply(1:length(obs_parent), function(locus) impute_one_site(locus, gen_error, p[locus], probs, obs_parent, obs_kids))
+    geno <- as.data.frame(matrix(unlist(res), ncol=3, byrow=TRUE))
+    names(geno) <- c("g0", "g1", "g2")
+    
+    return(geno)
+}
+
+#' @rdname impute_parent
+impute_one_site <- function(locus, gen_error, p_locus, probs, obs_parent, obs_kids){
+    obs_parent_probs <- as.numeric()
+    for(inferred_parent in 1:3){
+        #P(G'obs_parent|G)
+        pgg <- gen_error[inferred_parent, obs_parent[locus]+1] #+1 because obs_parent is 0,1, or 2
+        #P(G)
+        pg <- hw_probs(p_locus)[inferred_parent]
+        #P(kids|G) sum of logs instead of product
+        pkg <- sum(sapply(1:length(obs_kids), function(z){
+            ### take care of missing data
+            #if(progeny[[z]][[2]][locus] >= 0 & progeny[[z]][[2]][locus] <=2){
+            log(sum(probs[[inferred_parent]][, obs_kids[[z]][locus]+1]))
+            #}
+        } ))
+        obs_parent_probs[inferred_parent] <- pkg+log(pgg)+log(pg)
+    }
+    return(obs_parent_probs)
+}
+
+
+#' @rdname impute_parent
+parentgeno <- function(geno, oddratio=0.6931472, returnall=TRUE){ 
+    geno$OR <- apply(geno, 1, function(v){
+        n <- length(v)
+        return(max(v) - sort(v, partial=n-1)[n-1])  
+    })
+    geno$gmax <- apply(geno[, 1:3], 1, function(v){
+        return(which.max(v)-1)  
+    })
+    geno$gor <- 3 # 3 is missing data
+    geno[geno$OR > oddratio, ]$gor <- geno[geno$OR > oddratio, ]$gmax
+    
+    if(returnall){
+        return(geno)
+    }else{
+        if(is.null(oddratio)){
+            return(geno$gmax)   
+        }else{
+            return(geno$gor)
+        } 
+    }
+}
+
+
+
+
+
