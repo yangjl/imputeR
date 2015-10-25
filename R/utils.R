01cad4685e413c17e3cd5d77f6c9c7a35b58fbd2
Jeff Ross-Ibarra <rossibarra@gmail.com>
2015-10-25 00:06:23 -0700

    docs i hope

--- R/utils.R
+++ R/utils.R
@@ -59,7 +59,7 @@ error_mx <- function(hom.error, het.error){
     return(probs)
 }
 
-#' @rdname error_mx
+#' @rdname Utils
 gen_error_mat <- function(hom.error, het.error){
     mx <- matrix(c(1-hom.error,hom.error/2,hom.error/2,het.error/2,
                    1-het.error,het.error/2,hom.error/2,hom.error/2,1-hom.error),
@@ -70,3 +70,77 @@ gen_error_mat <- function(hom.error, het.error){
     return(mx)
 }
 
+#' @rdname Utils 
+# Create random haplotype with sfs
+ran.hap <- function(numloci,p){
+    sapply(1:numloci,function(x) rbinom(1,1,p[x]))
+}
+
+#' @rdname Utils 
+# Add error to diploid
+add_error<-function(diploid,hom.error,het.error){
+    hets_with_error=sample(which(diploid==1),round(het.error*length(which(diploid==1))))
+    hom0_with_error=sample(which(diploid==0),round(hom.error*length(which(diploid==0))))
+    hom2_with_error=sample(which(diploid==2),round(hom.error*length(which(diploid==2))))
+    diploid=replace(diploid,hets_with_error,sample(c(0,2),length(hets_with_error),replace=T)  )
+    ### error rate from (hom => het) == (hom1 => hom2)
+    diploid=replace(diploid,hom0_with_error,sample(c(1,2),length(hom0_with_error),replace=T)  )
+    diploid=replace(diploid,hom2_with_error,sample(c(1,0),length(hom2_with_error),replace=T)  )
+    return(diploid)
+}
+
+#' @rdname Utils 
+#Copy mom to kids with recombination
+copy.mom <- function(mom, co_mean){ 
+    co=rpois(1,co_mean) #crossovers
+    numloci=length(mom[[1]])
+    recp=c(1,sort(round(runif(co, min=2, max=numloci-1))), numloci+1) #position   
+    chrom=rbinom(1,1,.5)+1
+    kpiece=as.numeric()
+    hap <- c()
+    for(r in 1:(length(recp)-1)){
+        kpiece=c(kpiece,mom[[chrom]][recp[r]:(recp[r+1]-1)]) #copy 1->rec from mom
+        hap <- c(hap, chrom)
+        chrom=ifelse(chrom==1,2,1)  
+    }     
+    return(list(kpiece, data.frame(hap=hap, start=recp[-length(recp)], end=recp[-1]) ))
+}
+
+#' @rdname Utils 
+# add missing
+missing.idx <- function(nloci, imiss){
+    #hist(rbeta(10000, 2, 2))
+    if(imiss >= 1){
+        m <- rbeta(1, 2, 2)
+    }else{
+        m <- imiss
+    }
+    
+    ml <- sort(sample(1:nloci, size=round(m*nloci)))
+    return(ml)
+}
+
+
+#' @rdname Utils 
+# Returns a list of true [[1]] and observed [[2]] kid
+kid <- function(mom, dad, het.error, hom.error, rec=1.5, imiss=0.3, misscode=3){
+    if(rec==0){
+        k1=mom[[rbinom(1,1,.5)+1]]
+        k2=dad[[rbinom(1,1,.5)+1]]
+    } else{
+        k1=copy.mom(mom,rec) # list
+        k2=copy.mom(dad,rec)
+    }
+    true_kid=k1[[1]] + k2[[1]]
+    #return(list(true_kid,obs_kid))
+    
+    obs_kid <- add_error(true_kid, hom.error, het.error)
+    if(imiss > 0){
+        idx <- missing.idx(length(true_kid), imiss)
+        obs_kid <- replace(obs_kid, idx, misscode)
+    }
+    
+    info <- list(k1[[2]], k2[[2]])
+    simk <- data.frame(hap1=k1[[1]], hap2=k2[[1]], obs= obs_kid )
+    return(list(info, simk))
+}
