1aa5228045e4a7c15404fa2db28cf0fb7362c1de
yangjl0930@gmail.com <yangjl0930@gmail.com>
2015-10-29 10:19:56 +0800

    create Geno4imputeR object

--- R/loading_hdf5.R
+++ R/loading_hdf5.R
@@ -1,21 +1,22 @@
 #'
 #' \code{Load HDF5 file and recode the genotype matrix. } 
 #'
-#' Load HDF5 file and recode into `0, 1, 2, 3` format. In the genotype file, `0, 1,2` indicate the copy of alternative allele,
+#' Load HDF5 file and recode into `0, 1, 2, 3` format. In the genotype file, `0, 1, 2` indicate the copy of alternative allele,
 #' `3` indicate missing data. This function also calculate individual and locus missing rate.
 #' 
 #' @param h5file h5 file from tassel. For example, "teo.h5", 
 #' @param save.file RData object to save. For example, "out.RData".
-#' @return return three R objects: "genos", "info", "imiss". "genos", genotype matrix. "info", loci information, including missing
+#' @return return "Geno4imputeR" object with three slots: "genomx", "info", "imiss". 
+#' "genomx", genotype matrix. "info", loci information, including missing
 #' rate, maf, snp position, ref and alt alleles, etc. "imiss", individual plant missing rate.
 #'  
 #'   See \url{https://github.com/yangjl/imputeR/blob/master/vignettes/imputeR-vignette.pdf} for more details.
 #'   
 #' @examples
 #' # note this function requires at least 100G memeory to load teo.h5 file.
-#' loading_h5_recode(h5file="largedata/teo.h5", savefile="out.RData")
+#' obj <- loading_hdf5(h5file="largedata/teo.h5")
 #' 
-loading_h5_recode <- function(h5file="largedata/teo.h5", save.file="out.RData"){
+loading_hdf5 <- function(h5file="largedata/teo.h5"){
    
     #### load h5file
     teo <- initTasselHDF5(h5file, version="5")
@@ -52,5 +53,9 @@ loading_h5_recode <- function(h5file="largedata/teo.h5", save.file="out.RData"){
     info <- merge(info, lmiss, by.x="snpid", by.y="row.names")
     info <- merge(info, maf, by.x="snpid", by.y="row.names")
     
-    save(list=c("genos", "info", "imiss"), file="largedata/cj_data.Rdata")
+    obj <- new("Geno4imputeR", filename=file,
+               genomx = genos,
+               info = info,
+               imiss=imiss)
+    return(obj)
 }
