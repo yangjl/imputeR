01729ee4cf725767775393a5ffcb0e38eca2cae6
yangjl0930@gmail.com <yangjl0930@gmail.com>
2015-11-19 18:55:05 -0800

    more edits.

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -18,13 +18,18 @@ setClass("Geno4imputeR",
                     #samples="character",
                     ))
 
-#' An S4 class that stores links to Geno4imputeR objects, genotypes, loci missing rate, and MAF
-#'
-#' @slot genomx a matrix of bialleic genotypes, 0 ref, 1 het, 2 alt and 3 missing
-#' @slot info SNP information, including SNP ID, chr, position, ref and alt alleles, loci missing rate.
-#' @slot imiss individual plant missing rate
+#' A S4 class stores genomic data.
 #' 
-#' @exportClass Geno4imputeR
+#' Stores GBS data of progeny array and parental data, as well as pedigree information.
+#' In the GBS data, 0 ref, 1 het, 2 alt and 3 missing.
+#'
+#' @slot true_parents List of parents' read data. List index must consistent with pedigree.
+#' @slot gbs_parents List of parents' GBS data. 
+#' @slot true_kids List of progeny's readl data.
+#' @slot gbs_kids List of progeny's GBS data.
+#' @slot pedigree Pedigree information.
+#' 
+#' @exportClass GBS.array
 #' 
 setClass("GBS.array",
          representation=representation(
